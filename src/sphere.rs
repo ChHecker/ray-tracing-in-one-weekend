b22165cffb305612190c4d9653729976152486e4
ChHecker <39056704+ChHecker@users.noreply.github.com>
2023-02-22 07:55:24 +0100

    Made sphere return Option

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,10 +1,13 @@
 use crate::{Point3, Ray, Vec3};
 
-pub fn hits_sphere(ray: &Ray, center: &Point3, radius: f64) -> bool {
+pub fn hits_sphere(ray: &Ray, center: &Point3, radius: f64) -> Option<f64> {
     let oc = ray.origin() - *center;
     let a = ray.direction().dot(&ray.direction());
     let b = 2. * oc.dot(&ray.direction());
     let c = oc.dot(&oc) - radius * radius;
     let discriminant = b * b - 4. * a * c;
-    discriminant > 0.
+    if discriminant < 0. {
+        return None;
+    }
+    Some((-b - discriminant.sqrt()) / (2. * a))
 }
