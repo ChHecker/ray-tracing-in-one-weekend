0f5bc256ce7f9961974517828b98e7d716e2cf6d
ChHecker <christopher@hecker.info>
2023-02-22 13:36:01 +0100

    Added antialiasing

--- src/bin/chapter7.rs
+++ src/bin/chapter7.rs
@@ -1,5 +1,6 @@
 use std::{path::Path, rc::Rc};
 
+use rand::Rng;
 use raytracing_in_one_weekend::*;
 
 pub fn ray_color(world: &HittableList, ray: &Ray) -> Color3 {
@@ -17,6 +18,7 @@ fn main() {
     let aspect_ratio = 16. / 10.;
     let image_width: usize = 400;
     let image_height = (image_width as f64 / aspect_ratio) as usize;
+    let samples_per_pixel: usize = 100;
 
     // Camera
     let camera = Camera::new(aspect_ratio);
@@ -26,16 +28,23 @@ fn main() {
     world.push(Rc::new(Sphere::new(Point3::new(0., 0., -1.), 0.5)));
     world.push(Rc::new(Sphere::new(Point3::new(0., -100.5, -1.), 100.)));
 
+    let mut rng = rand::thread_rng();
+
     let mut ppm = Vec::<Color3>::new();
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
-            ppm.push(ray_color(&world, &camera.get_ray(u, v)));
+            let mut pixel_color = Color3::new(0., 0., 0.);
+            for _ in 0..samples_per_pixel {
+                let u = (i as f64 + rng.gen::<f64>()) / (image_width - 1) as f64;
+                let v = (j as f64 + rng.gen::<f64>()) / (image_height - 1) as f64;
+                pixel_color += ray_color(&world, &camera.get_ray(u, v));
+            }
+            pixel_color /= samples_per_pixel as f64;
+            ppm.push(pixel_color);
         }
     }
     write_ppm(
-        &Path::new("images/ppm6-4.ppm"),
+        &Path::new("images/ppm7.ppm"),
         (image_width, image_height),
         &ppm,
     )
