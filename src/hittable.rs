07e7f0314578478b847ad8d81d854bc5d8f2c224
ChHecker <christopher@hecker.info>
2023-02-22 10:21:46 +0100

    Added check whether ray hit front

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,15 +1,31 @@
-use crate::{Point3, Ray};
+use crate::{Point3, Ray, Vec3};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct HitRecord {
     point: Point3,
     normal: Point3,
     t: f64,
+    front_face: bool,
 }
 
 impl HitRecord {
-    pub fn new(point: Point3, normal: Point3, t: f64) -> Self {
-        HitRecord { point, normal, t }
+    pub fn new(point: Point3, normal: Point3, t: f64, front_face: bool) -> Self {
+        HitRecord {
+            point,
+            normal,
+            t,
+            front_face,
+        }
+    }
+
+    pub fn from_ray(point: Point3, normal: Point3, t: f64, ray: &Ray) -> Self {
+        let (front_face, normal) = HitRecord::face_normal(ray, normal);
+        HitRecord {
+            point,
+            normal,
+            t,
+            front_face,
+        }
     }
 
     pub fn point(&self) -> Point3 {
@@ -22,6 +38,20 @@ impl HitRecord {
 
     pub fn t(&self) -> f64 {
         self.t
+    }
+
+    pub fn front_face(&self) -> bool {
+        self.front_face
+    }
+
+    fn face_normal(ray: &Ray, outward_normal: Point3) -> (bool, Point3) {
+        let front_face = ray.direction().dot(&outward_normal) < 0.;
+        let normal = if front_face {
+            outward_normal
+        } else {
+            -outward_normal
+        };
+        (front_face, normal)
     }
 }
 
