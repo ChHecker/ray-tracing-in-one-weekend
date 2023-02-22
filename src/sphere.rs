963872e98de66b741074cf0ea0aafa28e18c0059
ChHecker <39056704+ChHecker@users.noreply.github.com>
2023-02-22 08:02:00 +0100

    Slightly simplified sphere

--- src/sphere.rs
+++ src/sphere.rs
@@ -2,12 +2,12 @@ use crate::{Point3, Ray, Vec3};
 
 pub fn hits_sphere(ray: &Ray, center: &Point3, radius: f64) -> Option<f64> {
     let oc = ray.origin() - *center;
-    let a = ray.direction().dot(&ray.direction());
-    let b = 2. * oc.dot(&ray.direction());
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4. * a * c;
+    let a = ray.direction().norm_sq();
+    let b_halves = oc.dot(&ray.direction());
+    let c = oc.norm_sq() - radius.powi(2);
+    let discriminant = b_halves.powi(2) - a * c;
     if discriminant < 0. {
         return None;
     }
-    Some((-b - discriminant.sqrt()) / (2. * a))
+    Some((-b_halves - discriminant.sqrt()) / a)
 }
