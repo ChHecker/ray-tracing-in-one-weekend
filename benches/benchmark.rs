9918733f0fd1bbfe2f9a552d28e908d73a42d040
ChHecker <39056704+ChHecker@users.noreply.github.com>
2023-03-09 20:14:08 +0100

    Fix benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,14 +1,13 @@
+use criterion::BatchSize::SmallInput;
 use criterion::{criterion_group, criterion_main, Criterion};
 use ray_tracing_in_one_weekend::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let camera = Camera::default();
-    let mut raytracer = Raytracer::new(camera, 160, 90, 10, 10);
+    let raytracer = Raytracer::new(camera, 160, 90, 10, 10);
 
     c.bench_function("Sphere", |b| {
-        b.iter(|| {
-            raytracer.render();
-        })
+        b.iter_batched(|| raytracer.clone(), |rt| rt.render(), SmallInput);
     });
 }
 
