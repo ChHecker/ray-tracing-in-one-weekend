fac98e3e358e4b0cc2fe5715a4f977bb10ce09df
ChHecker <christopher@hecker.info>
2023-03-09 23:07:02 +0100

    Fix benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,13 +1,25 @@
 use criterion::BatchSize::SmallInput;
 use criterion::{criterion_group, criterion_main, Criterion};
+use ray_tracing_in_one_weekend::materials::Lambertian;
+use ray_tracing_in_one_weekend::shapes::Sphere;
+use ray_tracing_in_one_weekend::textures::SolidColor;
 use ray_tracing_in_one_weekend::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let camera = Camera::default();
-    let raytracer = Raytracer::new(camera, 160, 90, 10, 10);
+    let texture = SolidColor::new(color![1., 1., 1.]);
+    let material = Lambertian::new(&texture);
 
     c.bench_function("Sphere", |b| {
-        b.iter_batched(|| raytracer.clone(), |rt| rt.render(), SmallInput);
+        b.iter_batched(
+            || {
+                let mut raytracer = Raytracer::new(Camera::default(), 160, 90, 10, 10);
+                let sphere = Sphere::new(point![0., 1., -1.], 1., &material);
+                raytracer.world.push(Box::new(sphere));
+                raytracer
+            },
+            |rt| rt.render(),
+            SmallInput,
+        );
     });
 }
 
