68427fbb9ee623220e7208d36eda0dba91251c77
ChHecker <39056704+ChHecker@users.noreply.github.com>
2023-03-10 11:18:33 +0100

    Fix bench

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,19 +2,20 @@ use criterion::BatchSize::SmallInput;
 use criterion::{criterion_group, criterion_main, Criterion};
 use ray_tracing_in_one_weekend::materials::Lambertian;
 use ray_tracing_in_one_weekend::shapes::Sphere;
-use ray_tracing_in_one_weekend::textures::SolidColor;
 use ray_tracing_in_one_weekend::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let texture = SolidColor::new(color![1., 1., 1.]);
-    let material = Lambertian::new(&texture);
+    let sphere = Sphere::new(
+        point![0., 1., -1.],
+        1.,
+        Lambertian::solid_color(color![1., 1., 1.]),
+    );
 
     c.bench_function("Sphere", |b| {
         b.iter_batched(
             || {
                 let mut raytracer = Raytracer::new(Camera::default(), 160, 90, 10, 10);
-                let sphere = Sphere::new(point![0., 1., -1.], 1., &material);
-                raytracer.world.push(Box::new(sphere));
+                raytracer.world.push(sphere.clone());
                 raytracer
             },
             |rt| rt.render(),
